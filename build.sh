1ff5cd9a6dc49e557498575dd3cc6f503926e29b
Luke Wahlmeier <lwahlmeier@gmail.com>
2018-05-23 08:54:04 -0600

    moved docker path

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t rt-freeswitch:latest .
+docker build -t docker-freeswitch:latest .
 
-FS_TMP=$(docker run --rm --entrypoint="" -it readytalk/rt-freeswitch:latest /usr/bin/freeswitch -version 2> /dev/null)
+FS_TMP=$(docker run --rm --entrypoint="" -it docker-freeswitch:latest /usr/bin/freeswitch -version 2> /dev/null)
 echo "--------------==="
 echo $FS_TMP
 echo "--------------==="
@@ -15,19 +15,19 @@ FS_A=(${FS_VERSION//./ })
 FS_A=(${FS_A[@]})
 FS_MM="${FS_A[0]}.${FS_A[1]}"
 
-docker tag rt-freeswitch:latest readytalk/rt-freeswitch:${FS_VERSION}
-docker tag rt-freeswitch:latest readytalk/rt-freeswitch:${FS_A[0]}.${FS_A[1]}
-docker tag rt-freeswitch:latest readytalk/rt-freeswitch:latest
+docker tag docker-freeswitch:latest readytalk/docker-freeswitch:${FS_VERSION}
+docker tag docker-freeswitch:latest readytalk/docker-freeswitch:${FS_A[0]}.${FS_A[1]}
+docker tag docker-freeswitch:latest readytalk/docker-freeswitch:latest
 echo "-----------------------"
-echo "Saved Tag \"rt-freeswitch:${FS_VERSION}\""
-echo "Saved Tag \"rt-freeswitch:${FS_MM}\""
-echo "Saved Tag \"rt-freeswitch:latest\""
+echo "Saved Tag \"docker-freeswitch:${FS_VERSION}\""
+echo "Saved Tag \"docker-freeswitch:${FS_MM}\""
+echo "Saved Tag \"docker-freeswitch:latest\""
 echo "-----------------------"
 
 if [[ ${TRAVIS} && "${TRAVIS_BRANCH}" == "master" && -n $DOCKER_USERNAME && -n $DOCKER_PASSWORD ]]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push readytalk/rt-freeswitch:${FS_VERSION}
-  docker push readytalk/rt-freeswitch:${FS_MM}
-  docker push readytalk/rt-freeswitch:latest
+  docker push readytalk/docker-freeswitch:${FS_VERSION}
+  docker push readytalk/docker-freeswitch:${FS_MM}
+  docker push readytalk/docker-freeswitch:latest
 fi
 
