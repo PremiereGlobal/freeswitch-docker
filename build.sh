8719c30b11f8b1da0dd7e418a5908ce2de3d562d
Luke Wahlmeier <lwahlmeier@gmail.com>
2019-02-16 13:33:07 -0700

    added docker 1.8/stretch image, added av support

--- build.sh
+++ build.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
 
-docker build -t freeswitch-docker:latest .
+docker build -t freeswitch-docker:1.6-latest . -f Dockerfile-1.6
+docker build -t freeswitch-docker:1.8-latest . -f Dockerfile-1.8
 
-FS_TMP=$(docker run --rm --entrypoint="" -it freeswitch-docker:latest /usr/bin/freeswitch -version 2> /dev/null)
+FS_16_TMP=$(docker run --rm --entrypoint="" -it freeswitch-docker:1.6-latest /usr/bin/freeswitch -version 2> /dev/null)
+FS_18_TMP=$(docker run --rm --entrypoint="" -it freeswitch-docker:1.8-latest /usr/bin/freeswitch -version 2> /dev/null)
 echo "--------------==="
-echo $FS_TMP
+echo $FS_16_TMP
+echo "--------------==="
+echo $FS_18_TMP
 echo "--------------==="
 
-FS_V_A=(${FS_TMP[@]})
-FS_VERSION=${FS_V_A[2]::-14}
-GIT_HASH=$(git rev-parse HEAD)
-GIT_COMMIT=${GIT_HASH::7}
-FS_A=(${FS_VERSION//./ })
-FS_A=(${FS_A[@]})
-FS_MM="${FS_A[0]}.${FS_A[1]}"
+FS_16_V_A=(${FS_16_TMP[@]})
+FS_16_VERSION=${FS_16_V_A[2]::-14}
+FS_16_A=(${FS_16_VERSION//./ })
+FS_16_A=(${FS_16_A[@]})
+FS_16_MM="${FS_16_A[0]}.${FS_16_A[1]}"
 
-docker tag freeswitch-docker:latest readytalk/freeswitch-docker:${FS_VERSION}
-docker tag freeswitch-docker:latest readytalk/freeswitch-docker:${FS_A[0]}.${FS_A[1]}
-docker tag freeswitch-docker:latest readytalk/freeswitch-docker:latest
+FS_18_V_A=(${FS_18_TMP[@]})
+FS_18_VERSION=${FS_18_V_A[2]::-14}
+FS_18_A=(${FS_18_VERSION//./ })
+FS_18_A=(${FS_18_A[@]})
+FS_18_MM="${FS_18_A[0]}.${FS_18_A[1]}"
+
+docker tag freeswitch-docker:1.6-latest readytalk/freeswitch-docker:${FS_16_VERSION}
+docker tag freeswitch-docker:1.6-latest readytalk/freeswitch-docker:${FS_16_MM}
+docker tag freeswitch-docker:1.8-latest readytalk/freeswitch-docker:${FS_18_VERSION}
+docker tag freeswitch-docker:1.8-latest readytalk/freeswitch-docker:${FS_18_MM}
+docker tag freeswitch-docker:1.8-latest readytalk/freeswitch-docker:latest
+
 echo "-----------------------"
-echo "Saved Tag \"freeswitch-docker:${FS_VERSION}\""
-echo "Saved Tag \"freeswitch-docker:${FS_MM}\""
-echo "Saved Tag \"freeswitch-docker:latest\""
+echo "Saved Tag \"readytalk/freeswitch-docker:${FS_16_VERSION}\""
+echo "Saved Tag \"readytalk/freeswitch-docker:${FS_16_MM}\""
+echo "Saved Tag \"readytalk/freeswitch-docker:${FS_18_VERSION}\""
+echo "Saved Tag \"readytalk/freeswitch-docker:${FS_18_MM}\""
+echo "Saved Tag \"readytalk/freeswitch-docker:latest\""
 echo "-----------------------"
 
 if [[ ${TRAVIS} && "${TRAVIS_BRANCH}" == "master" && -n $DOCKER_USERNAME && -n $DOCKER_PASSWORD ]]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push readytalk/freeswitch-docker:${FS_VERSION}
-  docker push readytalk/freeswitch-docker:${FS_MM}
+  docker push readytalk/freeswitch-docker:${FS_16_VERSION}
+  docker push readytalk/freeswitch-docker:${FS_16_MM}
+  docker push readytalk/freeswitch-docker:${FS_18_VERSION}
+  docker push readytalk/freeswitch-docker:${FS_18_MM}
   docker push readytalk/freeswitch-docker:latest
 fi
 
--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./build.sh
-docker run --rm -it --name fs1 -e "FS_XMLRPC_PORT=8081" -p 7443:7443  -p 7480:7480 -p 5060:5060 -p 5061:5061 -p 8081:8081 -p 8020:8020 freeswitch-docker:latest /bin/bash
+docker run --rm -it --name fs1 -e "FS_XMLRPC_PORT=8081" -p 7443:7443  -p 7480:7480 -p 5060:5060 -p 5061:5061 -p 8081:8081 -p 8020:8020 freeswitch-docker:latest
 
